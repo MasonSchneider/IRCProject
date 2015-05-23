68bc143aabf49a64ee3a92e1cd664b3af955eb06
WrightJosh <wrightj3@rose-hulman.edu>
2015-05-23 16:37:57 -0400

    forgot to add

---
+++ IRC Client/NewRoom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRC_Client
+{
+    public partial class NewRoom : Form
+    {
+        public NewRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOkRoom_Click(object sender, EventArgs e)
+        {
+            string room = this.txtRoom.Text;
+            Properties.Settings.Default["newRoom"] = room;
+            Properties.Settings.Default.Save();
+            this.Close();
+        }
+
+        private void btnCancelRoom_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
